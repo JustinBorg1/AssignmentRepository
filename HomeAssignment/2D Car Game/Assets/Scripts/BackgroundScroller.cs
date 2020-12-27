bb3a74d7cbbb62beb7d6a5fa9ca7f0d11606a634
JustinBorg1 <justin.borg.e21729@mcast.edu.mt>
2020-12-27 14:38:35 +0100

    Added prefabs and made scrolling background

---
+++ HomeAssignment/2D Car Game/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float ScrollSpeed = 0.2f;
+
+    //material from the texture
+    Material myMaterial;
+
+    Vector2 offSet;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //getting background material from Renderer component
+        myMaterial = GetComponent<Renderer>().material;
+
+        //move with the given speed on the y-axis
+        offSet = new Vector2(0f, ScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the material by offSet every frame
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
--- HomeAssignment/2D Car Game/Assets/Scripts/Player.cs
+++ HomeAssignment/2D Car Game/Assets/Scripts/Player.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f; 
+
+    float xMin, xMax;
+
+    float padding = 0.65f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+    //setting up the camera according to the boundaries
+    private void SetUpMoveBoundaries()
+    {
         
+        Camera gameCamera = Camera.main;
+
+        //xMin = 0, xMax = 1
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x+padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x-padding;
+
+    }
+
+    private  void Move()
+    {
+        //deltaX is being updated with movement occuring on the x-axis
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        var newXPos = transform.position.x + deltaX;
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+        
+        //updates the position of the Player
+        this.transform.position = new Vector2(newXPos, -5.75f);
+
     }
 }
